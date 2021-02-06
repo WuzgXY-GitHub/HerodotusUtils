548291d7094b54442c16ded0161fd4453bdab4c1
youyihj <youyi580@qq.com>
2021-02-06 16:10:32 +0800

    add IOreDictEntry#materialPart

---
+++ src/main/java/youyihj/herodotusutils/crafttweaker/MaterialPartOreExpansion.java
@@ -0,0 +1,32 @@
+package youyihj.herodotusutils.crafttweaker;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.oredict.IOreDictEntry;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenGetter;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.List;
+
+/**
+ * @author youyihj
+ */
+@ZenRegister
+@ZenExpansion("crafttweaker.oredict.IOreDictEntry")
+public class MaterialPartOreExpansion {
+    @ZenGetter
+    @ZenMethod
+    public static IItemStack materialPart(IOreDictEntry oreDictEntry) {
+        List<IItemStack> items = oreDictEntry.getItems();
+        if (items.isEmpty()) {
+            throw new NullPointerException(oreDictEntry + " is empty!");
+        }
+        for (IItemStack item : items) {
+            if (item.getDefinition().getOwner().equals("contenttweaker")) {
+                return item;
+            }
+        }
+        return oreDictEntry.getFirstItem();
+    }
+}
