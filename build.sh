bd426ab8d89ae7bcc30dc92c04ea4638e799e806
Gary Bryson Luis Jr <59724679+GBLodb@users.noreply.github.com>
2021-02-07 14:20:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ VERSION=1.0
 
 FILE_NAME="$BUILD-$VERSION.jar"
 
-cd home/runner/work/HerodotusUtils/HerodotusUtils/build/libs/
+cd home/runner/work/HerodotusUtils/build/libs/
 
 mv /$FILE_NAME artifacts/
