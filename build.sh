0c13a09f1712f3f3858cba79dca8dbbfb05790bf
youyihj <youyi580@qq.com>
2021-03-16 15:21:49 +0800

    hardcode filename "dev"

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 BUILD=herodotusutils
 VERSION=1.0
+DEV=dev
 
 FILE_NAME="$BUILD-$VERSION.jar"
+FILE_NAME_DEV="$BUILD-$DEV.jar"
 
 mv /home/runner/work/HerodotusUtils/HerodotusUtils/build/libs/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/
+mv /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME_DEV
