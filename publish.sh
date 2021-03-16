de10f162e64f007da7566790091c46a4efaadb6a
youyihj <youyi580@qq.com>
2021-03-16 12:59:37 +0800

    start publishing stuff

---
+++ publish.sh
@@ -0,0 +1,3 @@
+rm -f /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/build file here
+rm -f /home/runner/work/Herodotus/Herodotus/.minecraft/mods/*
+mv -f /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/* /home/runner/work/Herodotus/Herodotus/.minecraft/mods/
