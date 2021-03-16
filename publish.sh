5e32025b2b8af81967e753aa2354f751c12a5b89
youyihj <youyi580@qq.com>
2021-03-16 13:06:30 +0800

    Update gradle.yml

--- publish.sh
+++
@@ -1,3 +0,0 @@
-rm -f /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/build file here
-rm -f /home/runner/work/Herodotus/Herodotus/.minecraft/mods/*
-mv -f /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/* /home/runner/work/Herodotus/Herodotus/.minecraft/mods/
