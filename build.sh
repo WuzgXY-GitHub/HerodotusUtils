b00bce2c1b3127e67b10de753da4f68f101928e3
youyihj <youyi580@qq.com>
2021-03-17 13:14:06 +0800

    use run number

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ VERSION=1.0
 DEV=dev
 
 FILE_NAME="$BUILD-$VERSION.jar"
-FILE_NAME_DEV="$BUILD-$DEV.jar"
+FILE_NAME_DEV="$BUILD-$VERSION-build${GITHUB_RUN_NUMBER}.jar"
 
 mv /home/runner/work/HerodotusUtils/HerodotusUtils/build/libs/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/
 mv /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME_DEV
