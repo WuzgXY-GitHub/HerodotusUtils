9b53b4d8873c86f239ce5a8cc6d6d077fa5a8579
youyihj <youyi580@qq.com>
2021-03-17 16:17:02 +0800

    try to split jobs again

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 BUILD=herodotusutils
 VERSION=1.0
-DEV=dev
 
 FILE_NAME="$BUILD-$VERSION.jar"
 FILE_NAME_DEV="$BUILD-$VERSION-build${GITHUB_RUN_NUMBER}.jar"
 
-mv /home/runner/work/HerodotusUtils/HerodotusUtils/build/libs/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/
-mv /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME /home/runner/work/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME_DEV
+mv "$GITHUB_WORKSPACE/HerodotusUtils/build/libs/$FILE_NAME" "$GITHUB_WORKSPACE/HerodotusUtils/HerodotusUtils/artifacts/$FILE_NAME_DEV"
