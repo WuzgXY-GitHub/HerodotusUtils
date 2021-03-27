31492c6b48ae4492c2948a500266aa060fe4db61
youyihj <youyi580@qq.com>
2021-03-27 09:06:33 +0800

    fix: setBlockState mixin could not work at actual game

--- src/main/java/youyihj/herodotusutils/mixins/MixinTileMachineController.java
+++ src/main/java/youyihj/herodotusutils/mixins/MixinTileMachineController.java
@@ -6,37 +6,36 @@ import hellfirepvp.modularmachinery.common.machine.DynamicMachine;
 import hellfirepvp.modularmachinery.common.machine.TaggedPositionBlockArray;
 import hellfirepvp.modularmachinery.common.tiles.TileMachineController;
 import hellfirepvp.modularmachinery.common.tiles.base.TileEntityRestrictedTick;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import youyihj.herodotusutils.modsupport.modularmachinery.IDynamicMachinePatch;
 import youyihj.herodotusutils.modsupport.modularmachinery.event.MachineRecipeCompletedEvent;
 
-@Mixin(value = TileMachineController.class, remap = false)
+@Mixin(value = TileMachineController.class)
 public abstract class MixinTileMachineController extends TileEntityRestrictedTick {
 
-    @Shadow
+    @Shadow(remap = false)
     private DynamicMachine foundMachine;
 
-    @Shadow
+    @Shadow(remap = false)
     private EnumFacing patternRotation;
 
-    @Shadow
+    @Shadow(remap = false)
     private ActiveMachineRecipe activeRecipe;
 
-    @Inject(method = "checkStructure", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;)Z"), cancellable = true)
-    private void injectCheckStructure(CallbackInfo ci) {
-        ci.cancel();
-        if (foundMachine != null) {
-            world.setBlockState(pos, ((IDynamicMachinePatch) foundMachine).getController().getDefaultState().withProperty(BlockController.FACING, patternRotation));
-        }
+    @ModifyArg(method = "checkStructure", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;)Z"))
+    private IBlockState modifyStateToCheckStructure(IBlockState state) {
+        return ((IDynamicMachinePatch) foundMachine).getController().getDefaultState().withProperty(BlockController.FACING, patternRotation);
     }
 
-    @Inject(method = "matchesRotation", at = @At(value = "RETURN"), cancellable = true)
+    @Inject(method = "matchesRotation", at = @At(value = "RETURN"), cancellable = true, remap = false)
     private void injectMatchesRotation(TaggedPositionBlockArray pattern, DynamicMachine machine, CallbackInfoReturnable<Boolean> cir) {
         if (cir.getReturnValue()) {
             BlockController controller = ((IDynamicMachinePatch) machine).getController();
@@ -46,7 +45,7 @@ public abstract class MixinTileMachineController extends TileEntityRestrictedTic
         }
     }
 
-    @Inject(method = "doRestrictedTick", at = @At(value = "INVOKE", target = "Lhellfirepvp/modularmachinery/common/crafting/ActiveMachineRecipe;reset()V"))
+    @Inject(method = "doRestrictedTick", at = @At(value = "INVOKE", target = "Lhellfirepvp/modularmachinery/common/crafting/ActiveMachineRecipe;reset()V"), remap = false)
     private void injectDoRestrictedTick(CallbackInfo ci) {
         new MachineRecipeCompletedEvent(activeRecipe.getRecipe().getRegistryName(), foundMachine.getRegistryName(), pos, world).post();
     }
