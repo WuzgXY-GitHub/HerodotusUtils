9be574e9157e04bdbbca79809566eeba5b244cbc
youyihj <youyi580@qq.com>
2021-04-03 23:15:04 +0800

    tweak lang key format

--- src/main/java/youyihj/herodotusutils/mixins/MixinAgriPlant.java
+++ src/main/java/youyihj/herodotusutils/mixins/MixinAgriPlant.java
@@ -2,7 +2,6 @@ package youyihj.herodotusutils.mixins;
 
 import com.agricraft.agricore.plant.AgriPlant;
 import net.minecraft.util.text.translation.I18n;
-import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,23 +25,21 @@ public abstract class MixinAgriPlant {
 
     @Inject(method = "getSeedName", at = @At(value = "RETURN"), cancellable = true)
     public void injectGetSeedName(CallbackInfoReturnable<String> ci) {
-        String[] seedNameSpilt = this.seed_name.split(" ");
-        String translationKey = Arrays.stream(ArrayUtils.remove(seedNameSpilt, seedNameSpilt.length - 1))
+        String translationKey = Arrays.stream(this.seed_name.split(" "))
                 .map(String::toLowerCase)
                 .collect(Collectors.joining("_", "hdsutils.agricraft.", ""));
         if (I18n.canTranslate(translationKey)) {
-            ci.setReturnValue(I18n.translateToLocalFormatted("hdsutils.agricraft.seed", I18n.translateToLocal(translationKey)));
+            ci.setReturnValue(I18n.translateToLocal(translationKey));
         }
     }
 
     @Inject(method = "getPlantName", at = @At(value = "RETURN"), cancellable = true)
     public void injectGetPlantName(CallbackInfoReturnable<String> ci) {
-        String[] seedNameSpilt = this.plant_name.split(" ");
-        String translationKey = Arrays.stream(ArrayUtils.remove(seedNameSpilt, seedNameSpilt.length - 1))
+        String translationKey = Arrays.stream(this.plant_name.split(" "))
                 .map(String::toLowerCase)
                 .collect(Collectors.joining("_", "hdsutils.agricraft.", ""));
         if (I18n.canTranslate(translationKey)) {
-            ci.setReturnValue(I18n.translateToLocalFormatted("hdsutils.agricraft.plant", I18n.translateToLocal(translationKey)));
+            ci.setReturnValue(I18n.translateToLocal(translationKey));
         }
     }
 }
