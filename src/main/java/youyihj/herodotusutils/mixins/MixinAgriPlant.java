9bc4bfffff86d1c79032a46078ade321a02f300c
youyihj <youyi580@qq.com>
2021-04-03 23:24:08 +0800

    tweak lang key format again

--- src/main/java/youyihj/herodotusutils/mixins/MixinAgriPlant.java
+++ src/main/java/youyihj/herodotusutils/mixins/MixinAgriPlant.java
@@ -9,25 +9,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 @SuppressWarnings("deprecation")
 @Mixin(value = AgriPlant.class, remap = false)
 public abstract class MixinAgriPlant {
-    @Shadow
-    @Final
-    private String seed_name;
 
     @Shadow
     @Final
-    private String plant_name;
+    private String id;
 
     @Inject(method = "getSeedName", at = @At(value = "RETURN"), cancellable = true)
     public void injectGetSeedName(CallbackInfoReturnable<String> ci) {
-        String translationKey = Arrays.stream(this.seed_name.split(" "))
-                .map(String::toLowerCase)
-                .collect(Collectors.joining("_", "hdsutils.agricraft.", ""));
+
+        String translationKey = "hdsutils.agricraft." + this.id + ".seed";
         if (I18n.canTranslate(translationKey)) {
             ci.setReturnValue(I18n.translateToLocal(translationKey));
         }
@@ -35,9 +28,7 @@ public abstract class MixinAgriPlant {
 
     @Inject(method = "getPlantName", at = @At(value = "RETURN"), cancellable = true)
     public void injectGetPlantName(CallbackInfoReturnable<String> ci) {
-        String translationKey = Arrays.stream(this.plant_name.split(" "))
-                .map(String::toLowerCase)
-                .collect(Collectors.joining("_", "hdsutils.agricraft.", ""));
+        String translationKey = "hdsutils.agricraft." + this.id + ".plant";
         if (I18n.canTranslate(translationKey)) {
             ci.setReturnValue(I18n.translateToLocal(translationKey));
         }
