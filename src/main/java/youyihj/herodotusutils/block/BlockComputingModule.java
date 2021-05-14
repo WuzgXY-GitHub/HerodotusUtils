bb9f18ad234fca2977d423565517b1a5617722e7
youyihj <youyi580@qq.com>
2021-05-14 10:16:48 +0800

    right click to insert or extract item to computing module

--- src/main/java/youyihj/herodotusutils/block/BlockComputingModule.java
+++ src/main/java/youyihj/herodotusutils/block/BlockComputingModule.java
@@ -2,12 +2,21 @@ package youyihj.herodotusutils.block;
 
 import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 
 /**
  * @author youyihj
@@ -34,5 +43,31 @@ public class BlockComputingModule extends BlockCalculatorStructure {
     @Override
     public EnumPushReaction getMobilityFlag(IBlockState state) {
         return EnumPushReaction.BLOCK;
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (worldIn.isRemote)
+            return true;
+        ItemStack heldItem = playerIn.getHeldItem(hand);
+        Optional.ofNullable(worldIn.getTileEntity(pos))
+                .map(tileEntity -> tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
+                .map(itemHandler -> {
+                    if (heldItem.isEmpty()) {
+                        return itemHandler.extractItem(0, 1, false);
+                    } else {
+                        return ItemHandlerHelper.insertItem(itemHandler, heldItem, false);
+                    }
+                })
+                .ifPresent(item -> playerIn.setHeldItem(hand, item));
+        return true;
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        Optional.ofNullable(worldIn.getTileEntity(pos))
+                .map(tileEntity -> tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
+                .ifPresent(itemHandler -> InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY() + 0.5f, pos.getZ(), itemHandler.getStackInSlot(0).copy()));
+        super.breakBlock(worldIn, pos, state);
     }
 }
