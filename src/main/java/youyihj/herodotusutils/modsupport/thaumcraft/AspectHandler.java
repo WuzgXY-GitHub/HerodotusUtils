b6a406d5367e1a0cf1ba1c77e403212af6447588
youyihj <youyi580@qq.com>
2021-05-22 21:26:51 +0800

    fix aspect registry event

--- src/main/java/youyihj/herodotusutils/modsupport/thaumcraft/AspectHandler.java
+++ src/main/java/youyihj/herodotusutils/modsupport/thaumcraft/AspectHandler.java
@@ -1,31 +1,23 @@
 package youyihj.herodotusutils.modsupport.thaumcraft;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.aspects.AspectRegistryEvent;
+import youyihj.herodotusutils.HerodotusUtils;
 import youyihj.herodotusutils.block.BlockRegistry;
 
+@Mod.EventBusSubscriber
 public class AspectHandler {
     private static final Aspect HDS =
             new Aspect("Herodotus", 0XFF9D00,
-            new Aspect[]{Aspect.LIGHT, Aspect.ORDER},
-            new ResourceLocation("hdsutils:textures/icon.png"), 1);
-    private static boolean registered = false;
-    private static AspectHandler INSTANCE = new AspectHandler();
-
-    public static void register() {
-        if (!registered) {
-            MinecraftForge.EVENT_BUS.register(INSTANCE);
-            registered = true;
-        }
-    }
+                    new Aspect[]{Aspect.LIGHT, Aspect.ORDER},
+                    HerodotusUtils.rl("textures/icon.png"), 1);
 
     @SubscribeEvent
-    public void aspectRegisterEvent(AspectRegistryEvent event) {
+    public static void aspectRegisterEvent(AspectRegistryEvent event) {
         event.register.registerObjectTag(new ItemStack(BlockRegistry.RED_ORE),
                 new AspectList().add(Aspect.EARTH, 5).add(HDS, 5));
         event.register.registerObjectTag(new ItemStack(BlockRegistry.BLUE_ORE),
--- src/main/java/youyihj/herodotusutils/modsupport/thaumcraft/TCLoader.java
+++
@@ -1,9 +0,0 @@
-package youyihj.herodotusutils.modsupport.thaumcraft;
-
-import youyihj.herodotusutils.modsupport.thaumcraft.AspectHandler;
-
-public class TCLoader {
-    public static void loadTCCompat() {
-        AspectHandler.register();
-    }
-}
