5ef0ffee9716adad3b8f76ff41f915d639c3dedb
youyihj <youyi580@qq.com>
2021-05-26 20:33:15 +0800

    fix river grate removal issue

---
+++ src/main/java/youyihj/herodotusutils/mixins/MixinRiverGrateRemoveAction.java
@@ -0,0 +1,26 @@
+package youyihj.herodotusutils.mixins;
+
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.mc1120.item.MCItemStack;
+import dalapo.factech.auxiliary.MachineRecipes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * @author youyihj
+ */
+@Mixin(targets = "dalapo.factech.plugins.crafttweaker.RiverGrate$Remove", remap = false)
+public class MixinRiverGrateRemoveAction {
+    @Shadow
+    private IItemStack output;
+
+    /**
+     * @author youyihj
+     * @reason origin implementation is totally wrong
+     */
+    @Overwrite
+    public void apply() {
+        MachineRecipes.RIVER_GRATE.removeIf(recipe -> this.output.matches(new MCItemStack(recipe.getOutputStack())));
+    }
+}
