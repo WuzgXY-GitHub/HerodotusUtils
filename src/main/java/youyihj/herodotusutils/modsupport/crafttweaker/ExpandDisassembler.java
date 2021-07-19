4834f61b1468cfba3ae9d62a396bf78e739fa056
youyihj <youyi580@qq.com>
2021-07-19 16:16:40 +0800

    add ft disassembler tweaker using entity definition

---
+++ src/main/java/youyihj/herodotusutils/modsupport/crafttweaker/ExpandDisassembler.java
@@ -0,0 +1,100 @@
+package youyihj.herodotusutils.modsupport.crafttweaker;
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.entity.IEntityDefinition;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import dalapo.factech.auxiliary.MachineRecipes;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethodStatic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author youyihj
+ */
+@ZenExpansion("mods.factorytech.Disassembler")
+@ZenRegister
+public class ExpandDisassembler {
+    @ZenMethodStatic
+    public static void addRecipe(IEntityDefinition definition, IItemStack... stacks) {
+        CraftTweakerAPI.apply(new Add(definition, stacks));
+    }
+
+    @ZenMethodStatic
+    public static void removeRecipe(IEntityDefinition definition, IItemStack... stacks) {
+        CraftTweakerAPI.apply(new Remove(definition, stacks));
+    }
+
+    public static abstract class DisassemblerActionBase implements IAction {
+        protected final Class<? extends Entity> entityClass;
+        protected final IItemStack[] stacks;
+        protected final String entityCommandString;
+        private final String type;
+
+        protected DisassemblerActionBase(IEntityDefinition definition, IItemStack[] stacks, String type) {
+            this.entityClass = ((EntityEntry) definition.getInternal()).getEntityClass();
+            this.stacks = stacks;
+            this.entityCommandString = definition.toString();
+            this.type = type;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public void apply() {
+            if (EntityLiving.class.isAssignableFrom(entityClass)) {
+                CraftTweakerAPI.logWarning(entityCommandString + " is not a living entity!");
+            } else {
+                if (!MachineRecipes.DISASSEMBLER.containsKey(entityClass)) {
+                    MachineRecipes.DISASSEMBLER.put(((Class<? extends EntityLiving>) entityClass), new ArrayList<>());
+                }
+                List<ItemStack> itemStacks = MachineRecipes.DISASSEMBLER.get(entityClass);
+                tweakDroppingItems(itemStacks);
+            }
+        }
+
+        @Override
+        public String describe() {
+            return type + " Disassembler recipe for " + entityCommandString + " -> " + Arrays.stream(stacks).map(IItemStack::toCommandString).collect(Collectors.joining(", ", "[", "]"));
+        }
+
+        protected abstract void tweakDroppingItems(List<ItemStack> droppingItems);
+    }
+
+    public static class Add extends DisassemblerActionBase {
+
+        protected Add(IEntityDefinition definition, IItemStack[] stacks) {
+            super(definition, stacks, "Adding");
+        }
+
+        @Override
+        protected void tweakDroppingItems(List<ItemStack> droppingItems) {
+            droppingItems.addAll(Arrays.asList(CraftTweakerMC.getItemStacks(this.stacks)));
+        }
+    }
+
+    public static class Remove extends DisassemblerActionBase {
+
+        protected Remove(IEntityDefinition definition, IItemStack[] stacks) {
+            super(definition, stacks, "Removing");
+        }
+
+        @Override
+        protected void tweakDroppingItems(List<ItemStack> droppingItems) {
+            List<ItemStack> collect = CraftTweakerMC.getIItemStackList(droppingItems).stream()
+                    .filter(stack -> Arrays.stream(stacks).anyMatch(stack::matches))
+                    .map(CraftTweakerMC::getItemStack)
+                    .collect(Collectors.toList());
+            droppingItems.removeAll(collect);
+        }
+    }
+}
