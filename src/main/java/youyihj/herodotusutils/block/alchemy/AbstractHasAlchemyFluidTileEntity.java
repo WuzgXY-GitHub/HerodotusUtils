bd4c0edf970e5272e66717c7a3002bb6c7d77a7c
youyihj <youyi580@qq.com>
2021-07-19 21:51:00 +0800

    should serialize IHasAlchemyFluid TileEntity

--- src/main/java/youyihj/herodotusutils/block/alchemy/AbstractHasAlchemyFluidTileEntity.java
+++ src/main/java/youyihj/herodotusutils/block/alchemy/AbstractHasAlchemyFluidTileEntity.java
@@ -1,7 +1,9 @@
 package youyihj.herodotusutils.block.alchemy;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import youyihj.herodotusutils.alchemy.IHasAlchemyFluid;
 
 /**
@@ -10,6 +12,21 @@ import youyihj.herodotusutils.alchemy.IHasAlchemyFluid;
 public abstract class AbstractHasAlchemyFluidTileEntity extends AbstractPipeTileEntity implements IHasAlchemyFluid {
     private Fluid content;
     private Fluid cachedContent;
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        content = FluidRegistry.getFluid(compound.getString("fluid"));
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        if (content != null) {
+            compound.setString("fluid", content.getName());
+        }
+        return compound;
+    }
 
     @Override
     public Fluid getContainedFluid() {
--- src/main/java/youyihj/herodotusutils/block/alchemy/TilePlainAlchemyTransporter.java
+++
@@ -1,7 +0,0 @@
-package youyihj.herodotusutils.block.alchemy;
-
-/**
- * @author youyihj
- */
-public class TilePlainAlchemyTransporter {
-}
