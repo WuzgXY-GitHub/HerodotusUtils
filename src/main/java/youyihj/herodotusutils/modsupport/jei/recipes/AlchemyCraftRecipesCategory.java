db53b01c87d85c60e1084b2ddcb54fcd06b872e3
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-07 02:08:35 +0800

    layouts and Item render

--- src/main/java/youyihj/herodotusutils/modsupport/jei/recipes/AlchemyCraftRecipesCategory.java
+++ src/main/java/youyihj/herodotusutils/modsupport/jei/recipes/AlchemyCraftRecipesCategory.java
@@ -1,26 +1,38 @@
 package youyihj.herodotusutils.modsupport.jei.recipes;
 
+import com.google.common.collect.Lists;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiFluidStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeCategory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.translation.I18n;
+import org.apache.commons.lang3.tuple.Pair;
 import youyihj.herodotusutils.HerodotusUtils;
 import youyihj.herodotusutils.block.alchemy.BlockAlchemyCrafter;
+import youyihj.herodotusutils.util.Util;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class AlchemyCraftRecipesCategory implements IRecipeCategory<AlchemyCraftRecipesWrapper> {
 
     private final IDrawable icon;
     private final IDrawable background;
 
+    private final List<Pair<Integer, Integer>> inputLayouts = Lists.newArrayList(
+            Pair.of(72, 0),
+            Pair.of(72, 94),
+            Pair.of(22, 45),
+            Pair.of(122, 45));
+    private final Pair<Integer, Integer> outputLayout = Pair.of(72, 45);
+
     public AlchemyCraftRecipesCategory(IGuiHelper guiHelper) {
         this.icon = guiHelper.createDrawableIngredient(new ItemStack(BlockAlchemyCrafter.INSTANCE));
-        this.background = guiHelper.createBlankDrawable(200, 200);
+        this.background = guiHelper.createBlankDrawable(160, 110);
     }
 
     @Override
@@ -30,7 +42,7 @@ public class AlchemyCraftRecipesCategory implements IRecipeCategory<AlchemyCraft
 
     @Override
     public String getTitle() {
-        return I18n.translateToLocal("hdsutils.jei.alchemy_craft_title");
+        return I18n.translateToLocal("hdsutils.jei.alchemy_craft.title");
     }
 
     @Override
@@ -52,13 +64,17 @@ public class AlchemyCraftRecipesCategory implements IRecipeCategory<AlchemyCraft
     @Override
     public void setRecipe(IRecipeLayout recipeLayout, AlchemyCraftRecipesWrapper recipeWrapper, IIngredients ingredients) {
         IGuiFluidStackGroup group = recipeLayout.getFluidStacks();
-
-        // todo layout
-        group.init(0, true, 20, 20);
-        group.init(1, true, 40, 40);
-        group.init(2, true, 60, 60);
-        group.init(3, true, 80, 80);
-        group.init(4, false, 100, 100);
+        for (int i = 0; i < inputLayouts.size(); i++) {
+            Pair<Integer, Integer> input = inputLayouts.get(i);
+            group.init(i, true, input.getLeft(), input.getValue());
+        }
+        group.init(inputLayouts.size(), false, outputLayout.getLeft(), outputLayout.getRight());
         group.set(ingredients);
+    }
+
+    @Override
+    public void drawExtras(Minecraft minecraft) {
+        Util.renderItem(minecraft, outputLayout.getLeft(), outputLayout.getRight(), false);
+        inputLayouts.forEach(p -> Util.renderItem(minecraft, p.getLeft(), p.getValue(), true));
     }
 }
--- src/main/java/youyihj/herodotusutils/util/Util.java
+++ src/main/java/youyihj/herodotusutils/util/Util.java
@@ -4,14 +4,21 @@ import crafttweaker.api.data.DataMap;
 import crafttweaker.api.data.IData;
 import it.unimi.dsi.fastutil.ints.IntCollection;
 import it.unimi.dsi.fastutil.ints.IntIterator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import youyihj.herodotusutils.HerodotusUtils;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author youyihj
@@ -45,11 +52,18 @@ public final class Util {
         return Optional.ofNullable(world.getTileEntity(pos));
     }
 
-    public static List<FluidStack> getDefaultFluidStacks(Fluid[] fluid) {
-        return Arrays.stream(fluid).map(Util::getDefaultFluidStack).collect(Collectors.toList());
-    }
-
     public static FluidStack getDefaultFluidStack(Fluid fluid) {
         return new FluidStack(fluid, 1000);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void renderItem(Minecraft minecraft, int x, int y, boolean isInput) {
+        GlStateManager.enableAlpha();
+        minecraft.getTextureManager().bindTexture(HerodotusUtils.rl("textures/gui/jei/jei_default.png"));
+        if (isInput)
+            Gui.drawModalRectWithCustomSizedTexture(x - 1, y - 1, 0, 0, 18, 18, 256, 256);
+        else
+            Gui.drawModalRectWithCustomSizedTexture(x - 5, y - 5, 80, 0, 27, 27, 256, 256);
+        GlStateManager.disableAlpha();
     }
 }
