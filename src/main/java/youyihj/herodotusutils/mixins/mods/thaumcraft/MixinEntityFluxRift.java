a7ff01e11bc7b0b37c22a4845eef98a26c67f8fc
youyihj <youyi580@qq.com>
2021-08-09 19:45:19 +0800

    rift minus players' max health

--- src/main/java/youyihj/herodotusutils/mixins/mods/thaumcraft/MixinEntityFluxRift.java
+++ src/main/java/youyihj/herodotusutils/mixins/mods/thaumcraft/MixinEntityFluxRift.java
@@ -1,13 +1,23 @@
 package youyihj.herodotusutils.mixins.mods.thaumcraft;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import thaumcraft.common.entities.EntityFluxRift;
+import youyihj.herodotusutils.util.Capabilities;
+import youyihj.herodotusutils.util.ITaint;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 /**
  * @author youyihj
@@ -21,9 +31,31 @@ public abstract class MixinEntityFluxRift extends Entity {
     }
 
     private static final DamageSource RIFT = new DamageSource("rift").setDamageBypassesArmor().setMagicDamage();
+    private static final UUID RIFT_MINUS_MAX_HEALTH_MODIFIER = UUID.nameUUIDFromBytes("rift".getBytes(StandardCharsets.UTF_8));
 
-    @ModifyArg(method = "onUpdate()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z"))
-    public DamageSource modifyDamageSource(DamageSource source) {
-        return RIFT;
+    @Redirect(method = "onUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z"))
+    public boolean injectOnUpdate(Entity entity, DamageSource source, float amount) {
+        boolean flag = false;
+        boolean attackResult = false;
+        if (entity instanceof EntityLivingBase) {
+            EntityLivingBase living = ((EntityLivingBase) entity);
+            ITaint taint = living.getCapability(Capabilities.TAINT_CAPABILITY, null);
+            if (taint != null && taint.moreThanScale(0.5f)) {
+                amount *= 2;
+                flag = true;
+            }
+            attackResult = living.attackEntityFrom(RIFT, amount);
+            if (!flag && attackResult) {
+                IAttributeInstance maxHealth = living.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+                AttributeModifier modifier = maxHealth.getModifier(RIFT_MINUS_MAX_HEALTH_MODIFIER);
+                double decrease = -amount;
+                if (modifier != null) {
+                    decrease += modifier.getAmount();
+                    maxHealth.removeModifier(RIFT_MINUS_MAX_HEALTH_MODIFIER);
+                }
+                maxHealth.applyModifier(new AttributeModifier(RIFT_MINUS_MAX_HEALTH_MODIFIER, "Rift Minus", decrease, Constants.AttributeModifierOperation.ADD));
+            }
+        }
+        return attackResult;
     }
 }
