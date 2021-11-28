780f0ce60996f0c3b6b2ca240677565bf81ebeed
youyihj <youyi580@qq.com>
2021-11-28 23:20:52 +0800

    essence textures

--- src/main/java/youyihj/herodotusutils/modsupport/jei/render/AlchemyEssenceRender.java
+++ src/main/java/youyihj/herodotusutils/modsupport/jei/render/AlchemyEssenceRender.java
@@ -2,8 +2,11 @@ package youyihj.herodotusutils.modsupport.jei.render;
 
 import mezz.jei.api.ingredients.IIngredientRenderer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.util.ResourceLocation;
+import youyihj.herodotusutils.HerodotusUtils;
 import youyihj.herodotusutils.alchemy.AlchemyEssenceStack;
 
 import javax.annotation.Nullable;
@@ -14,12 +17,18 @@ import java.util.List;
  * @author youyihj
  */
 public class AlchemyEssenceRender implements IIngredientRenderer<AlchemyEssenceStack> {
+    private final ResourceLocation runesTexture = HerodotusUtils.rl("textures/gui/jei/runes.png");
+
     @Override
     public void render(Minecraft minecraft, int xPosition, int yPosition, @Nullable AlchemyEssenceStack ingredient) {
-        // TODO: change it to texture
         if (ingredient != null) {
-            FontRenderer fontRenderer = getFontRenderer(minecraft, ingredient);
-            fontRenderer.drawString(ingredient.getDisplayName(), xPosition, yPosition, 0x0000ff);
+            GlStateManager.enableAlpha();
+            minecraft.getTextureManager().bindTexture(runesTexture);
+            int index = ingredient.getEssence().getIndex();
+            Gui.drawScaledCustomSizeModalRect(xPosition, yPosition, (index & 15) * 32, (index >> 4) * 32, 32, 32, 16, 16, 512, 512);
+            if (ingredient.getCount() > 1) {
+                getFontRenderer(minecraft, ingredient).drawString(String.valueOf(ingredient.getCount()), xPosition + 13, yPosition + 9, -1);
+            }
         }
     }
 
