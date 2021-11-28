e4ab080193f1a2bfdd2a66e9b9bf9f2a5731f470
youyihj <youyi580@qq.com>
2021-11-28 14:12:12 +0800

    remove old recipes

--- src/main/java/youyihj/herodotusutils/modsupport/jei/JeiPlugin.java
+++ src/main/java/youyihj/herodotusutils/modsupport/jei/JeiPlugin.java
@@ -1,25 +1,13 @@
 package youyihj.herodotusutils.modsupport.jei;
 
-import mezz.jei.Internal;
 import mezz.jei.api.*;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
-import net.minecraft.item.ItemStack;
-import youyihj.herodotusutils.HerodotusUtils;
-import youyihj.herodotusutils.block.alchemy.BlockAlchemyCrafter;
-import youyihj.herodotusutils.block.alchemy.BlockAlchemySeparator;
 import youyihj.herodotusutils.modsupport.jei.helper.ImpetusHelper;
-import youyihj.herodotusutils.modsupport.jei.recipes.alchemycraft.AlchemyCraftRecipesCategory;
-import youyihj.herodotusutils.modsupport.jei.recipes.alchemycraft.AlchemyCraftRecipesWrapper;
-import youyihj.herodotusutils.modsupport.jei.recipes.alchemyseparate.AlchemySeparateRecipesCategory;
-import youyihj.herodotusutils.modsupport.jei.recipes.alchemyseparate.AlchemySeparateRecipesWrapper;
 import youyihj.herodotusutils.modsupport.jei.render.ImpetusRender;
 import youyihj.herodotusutils.modsupport.modularmachinery.crafting.ingredient.Impetus;
-import youyihj.herodotusutils.recipe.AlchemyRecipes;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author youyihj
@@ -31,38 +19,14 @@ public class JeiPlugin implements IModPlugin {
     @Override
     public void register(IModRegistry registry) {
         JEI_HELPER = registry.getJeiHelpers();
-
-        if (!AlchemyRecipes.craftRecipes.isEmpty()) {
-            registry.addRecipeCatalyst(new ItemStack(BlockAlchemyCrafter.INSTANCE), "AlchemyCraftJEI");
-
-            List<AlchemyCraftRecipesWrapper> alchemyCraftRecipesWrappers =
-                    AlchemyRecipes.craftRecipes.stream().map(AlchemyCraftRecipesWrapper::new).collect(Collectors.toList());
-            registry.addRecipes(alchemyCraftRecipesWrappers, "AlchemyCraftJEI");
-        }
-        if (!AlchemyRecipes.separateRecipes.isEmpty()) {
-            registry.addRecipeCatalyst(new ItemStack(BlockAlchemySeparator.INSTANCE), "AlchemySeparateJEI");
-
-            List<AlchemySeparateRecipesWrapper> alchemySeparateRecipesWrappers =
-                    AlchemyRecipes.separateRecipes.stream().map(AlchemySeparateRecipesWrapper::new).collect(Collectors.toList());
-            registry.addRecipes(alchemySeparateRecipesWrappers, "AlchemySeparateJEI");
-        }
     }
 
     @Override
     public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
-        if (!AlchemyRecipes.separateRecipes.isEmpty() || !AlchemyRecipes.craftRecipes.isEmpty()) {
-            HerodotusUtils.logger.info("JEI Support Loading");
-        }
     }
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registry) {
-        if (!AlchemyRecipes.craftRecipes.isEmpty()) {
-            registry.addRecipeCategories(new AlchemyCraftRecipesCategory(Internal.getHelpers().getGuiHelper()));
-        }
-        if (!AlchemyRecipes.separateRecipes.isEmpty()) {
-            registry.addRecipeCategories(new AlchemySeparateRecipesCategory(Internal.getHelpers().getGuiHelper()));
-        }
     }
 
     @Override
--- src/main/java/youyihj/herodotusutils/recipe/AlchemyRecipes.java
+++ src/main/java/youyihj/herodotusutils/recipe/AlchemyRecipes.java
@@ -2,13 +2,9 @@ package youyihj.herodotusutils.recipe;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
-import crafttweaker.CraftTweakerAPI;
-import crafttweaker.IAction;
-import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.liquid.ILiquidStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import net.minecraftforge.fluids.Fluid;
-import org.apache.commons.lang3.ArrayUtils;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import youyihj.herodotusutils.alchemy.AlchemyEssence;
@@ -16,19 +12,13 @@ import youyihj.herodotusutils.alchemy.AlchemyEssenceStack;
 import youyihj.herodotusutils.alchemy.AlchemyFluid;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author youyihj
  */
 @ZenClass("mods.hdsutils.Alchemy")
 public class AlchemyRecipes {
-    public static final List<Craft> craftRecipes = new ArrayList<>();
-    public static final List<Separate> separateRecipes = new ArrayList<>();
     private static final BiMap<Fluid, AlchemyFluid> normalFluidToAlchemyMap = HashBiMap.create();
 
     @ZenMethod
@@ -53,134 +43,4 @@ public class AlchemyRecipes {
         return normalFluidToAlchemyMap.get(fluid);
     }
 
-    @ZenMethod
-    public static void addCraftingRecipe(ILiquidStack output, ILiquidStack[] input) {
-        CraftTweakerAPI.apply(new AddCraftingRecipeAction(output, input));
-    }
-
-    @ZenMethod
-    public static void addSeparatingRecipe(ILiquidStack input, ILiquidStack[] output) {
-        CraftTweakerAPI.apply(new AddSeparatingRecipeAction(input, output));
-    }
-
-    @Nullable
-    public static Fluid getCraftingOutputFor(Fluid[] input) {
-        return craftRecipes.stream()
-                .filter(recipe -> recipe.checkInput(input))
-                .map(Craft::getOutput)
-                .findFirst()
-                .orElse(null);
-    }
-
-    @Nullable
-    public static Fluid[] getSeparatingOutputFor(Fluid input) {
-        return separateRecipes.stream()
-                .filter(recipe -> recipe.getInput() == input)
-                .map(Separate::getOutput)
-                .findFirst()
-                .orElse(null);
-    }
-
-    public static class Craft {
-        private final Fluid output;
-        private final Fluid[] input;
-
-        public Craft(Fluid output, Fluid[] input) {
-            this.output = output;
-            this.input = input;
-        }
-
-        public Fluid getOutput() {
-            return output;
-        }
-
-        public Fluid[] getInput() {
-            return input;
-        }
-
-        public boolean checkInput(Fluid[] toCheck) {
-            if (input.length != toCheck.length)
-                return false;
-            for (Fluid fluid : input) {
-                if (!ArrayUtils.contains(toCheck, fluid))
-                    return false;
-            }
-            return true;
-        }
-    }
-
-    public static class Separate {
-        private final Fluid input;
-        private final Fluid[] output;
-
-        public Separate(Fluid input, Fluid[] output) {
-            this.input = input;
-            this.output = output;
-        }
-
-        public Fluid getInput() {
-            return input;
-        }
-
-        public Fluid[] getOutput() {
-            return output;
-        }
-    }
-
-    public static class AddCraftingRecipeAction implements IAction {
-        private final ILiquidStack output;
-        private final ILiquidStack[] input;
-
-        public AddCraftingRecipeAction(ILiquidStack output, ILiquidStack[] input) {
-            this.output = output;
-            this.input = input;
-        }
-
-        @Override
-        public void apply() {
-            if (input.length > 4) {
-                throw new IllegalArgumentException();
-            }
-            Fluid mcOutput = CraftTweakerMC.getFluid(output.getDefinition());
-            Fluid[] mcInput = Arrays.stream(input)
-                    .map(ILiquidStack::getDefinition)
-                    .map(CraftTweakerMC::getFluid)
-                    .toArray(Fluid[]::new);
-            craftRecipes.add(new Craft(mcOutput, mcInput));
-        }
-
-        @Override
-        public String describe() {
-            return "Adding a new crafting alchemy recipe, output: " + output.toCommandString() + "input: " + Arrays.stream(input).map(IIngredient::toCommandString).collect(Collectors.joining(", ", "[", "]"));
-        }
-    }
-
-    public static class AddSeparatingRecipeAction implements IAction {
-        private final ILiquidStack input;
-        private final ILiquidStack[] output;
-
-        public AddSeparatingRecipeAction(ILiquidStack input, ILiquidStack[] output) {
-            this.input = input;
-            this.output = output;
-        }
-
-        @Override
-        public void apply() {
-            if (output.length > 4) {
-                throw new IllegalArgumentException();
-            }
-
-            Fluid mcInput = CraftTweakerMC.getFluid(input.getDefinition());
-            Fluid[] mcOutput = Arrays.stream(output)
-                    .map(ILiquidStack::getDefinition)
-                    .map(CraftTweakerMC::getFluid)
-                    .toArray(Fluid[]::new);
-            separateRecipes.add(new Separate(mcInput, mcOutput));
-        }
-
-        @Override
-        public String describe() {
-            return "Adding a new separating alchemy recipe, input: " + input.toCommandString() + "input: " + Arrays.stream(output).map(IIngredient::toCommandString).collect(Collectors.joining(", ", "[", "]"));
-        }
-    }
 }
