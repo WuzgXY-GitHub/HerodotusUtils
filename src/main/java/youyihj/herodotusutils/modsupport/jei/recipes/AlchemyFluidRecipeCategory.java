90a68c940d4817a91dece6d768f5b9e6010407d1
youyihj <youyi580@qq.com>
2021-11-28 20:55:45 +0800

    round

--- src/main/java/youyihj/herodotusutils/modsupport/jei/recipes/AlchemyFluidRecipeCategory.java
+++ src/main/java/youyihj/herodotusutils/modsupport/jei/recipes/AlchemyFluidRecipeCategory.java
@@ -7,8 +7,8 @@ import mezz.jei.api.gui.IGuiIngredientGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeCategory;
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.fluids.FluidStack;
 import youyihj.herodotusutils.HerodotusUtils;
@@ -16,7 +16,6 @@ import youyihj.herodotusutils.alchemy.AlchemyEssenceStack;
 import youyihj.herodotusutils.alchemy.IAlchemyExternalHatch;
 import youyihj.herodotusutils.block.alchemy.BlockAlchemyController;
 import youyihj.herodotusutils.modsupport.jei.ModIngredientTypes;
-import youyihj.herodotusutils.util.Util;
 
 import javax.annotation.Nullable;
 
@@ -26,10 +25,12 @@ import javax.annotation.Nullable;
 public class AlchemyFluidRecipeCategory implements IRecipeCategory<AlchemyFluidRecipeWrapper> {
     private final IDrawable icon;
     private final IDrawable background;
+    private final int width = 100;
+    private final int height = 100;
 
     public AlchemyFluidRecipeCategory(IGuiHelper guiHelper) {
         this.icon = guiHelper.createDrawableIngredient(new ItemStack(BlockAlchemyController.ITEM_BLOCK));
-        this.background = guiHelper.createBlankDrawable(120, 32);
+        this.background = guiHelper.createBlankDrawable(width, height);
     }
 
     @Override
@@ -61,19 +62,19 @@ public class AlchemyFluidRecipeCategory implements IRecipeCategory<AlchemyFluidR
     @Override
     public void setRecipe(IRecipeLayout recipeLayout, AlchemyFluidRecipeWrapper recipeWrapper, IIngredients ingredients) {
         AlchemyEssenceStack[] essences = recipeWrapper.getAlchemyFluid().getEssenceStacks().toArray(new AlchemyEssenceStack[0]);
-        int size = essences.length;
         IGuiFluidStackGroup fluidGroup = recipeLayout.getFluidStacks();
         IGuiIngredientGroup<AlchemyEssenceStack> essenceGroup = recipeLayout.getIngredientsGroup(ModIngredientTypes.ALCHEMY_ESSENCE);
-        for (int i = 0; i < size; i++) {
-            essenceGroup.init(i, true, 78 - 16 * (size - i), 8);
+        float f = (float) (Math.PI * 2 / essences.length);
+        float radian = 0.0f;
+        for (int i = 0; i < essences.length; i++) {
+            int essenceRoundRadius = 40;
+            int x = width / 2 + Math.round(MathHelper.sin(radian) * essenceRoundRadius) - 8;
+            int y = height / 2 - Math.round(MathHelper.cos(radian) * essenceRoundRadius) - 8;
+            essenceGroup.init(i, true, x, y);
             essenceGroup.set(i, essences[i]);
+            radian += f;
         }
-        fluidGroup.init(0, false, 104, 8);
+        fluidGroup.init(0, false, width / 2 - 8, height / 2 - 8);
         fluidGroup.set(0, new FluidStack(recipeWrapper.getFluid(), IAlchemyExternalHatch.FLUID_UNIT));
-    }
-
-    @Override
-    public void drawExtras(Minecraft minecraft) {
-        Util.renderArrow(minecraft, 78, 8, 0);
     }
 }
